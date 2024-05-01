c65ec2ae320a5397cd670561b3d18fc72662e085
ramber322 <abdulmalikramber.202200383@gmail.com>
2024-05-02 01:39:19 +0800

    PIATOS

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,13 +6,7 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function restrictAdmin () {
-
-        if (auth()->user()->role === 'admin') {
-            return redirect('admin-dashboard');
-        }
-
-    }
+ 
 
     public function index () {
         return view ('admin.dashboard');
